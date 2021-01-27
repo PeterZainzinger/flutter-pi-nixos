114fdfeb3fe60582cbaf12c9ce29dc6ce63d8c10
Peter Zainzinger <peterzainzinger@gmail.com>
2021-01-27 09:07:18 +0100

    inital commit

---
+++ flake.nix
@@ -0,0 +1,99 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    # flutter 1.24
+    unstable.url =
+      "github:thiagokokada/nixpkgs/49a37a887c96d68e23993778e8330b1f36852473";
+  };
+  outputs = { self, nixpkgs, flake-utils, unstable }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = (import unstable {
+          system = system;
+          config = { allowUnfree = true; };
+        });
+
+      in {
+
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            clang
+            pkg-config
+            libdrm
+            mesa
+            libGL
+            systemd
+            libinput
+            libxkbcommon
+            xorg.libX11
+            cmake
+            python38
+          ];
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            libdrm
+            mesa
+            libGL
+            systemd
+            libinput
+            libxkbcommon
+            xorg.libX11
+            python38
+            breakpointHook
+          ];
+
+          #shellHook = "";
+        };
+
+        defaultPackage = pkgs.stdenv.mkDerivation {
+          name = "flutter-pi";
+          src = pkgs.fetchFromGitHub {
+            url = "https://github.com//.git";
+            owner = "ardera";
+            repo = "flutter-pi";
+            rev = "133600ca46892e59b679f31378a7be1dc5aaa4d8";
+            sha256 = "sha256-udpbSL8xnzf20A1pYYNlYjdp8ME8AVaAkMMiw53K6CU=";
+          };
+
+          buildInputs = with pkgs; [
+            clang
+            pkg-config
+            libdrm
+            mesa
+            libGL
+            systemd
+            libinput
+            libxkbcommon
+            xorg.libX11
+            cmake
+            python38
+          ];
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            libdrm
+            mesa
+            libGL
+            systemd
+            libinput
+            libxkbcommon
+            xorg.libX11
+            python38
+            breakpointHook
+          ];
+
+          configurePhase = ''
+            cmake --trace . 
+          '';
+
+          buildPhase = ''
+            make
+          '';
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp flutter-pi $out/bin
+                      '';
+
+        };
+      });
+}
